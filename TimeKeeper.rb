9a5935e2ca01011b990254fe0d87487f5dbaed6c
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-05 22:11:40 +0900

    ログファイルを元に、時間差を取得するメソッドを実装

--- TimeKeeper.rb
+++ TimeKeeper.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'date'
+require 'time'
 
 class TimeKeeper
 
@@ -12,6 +13,29 @@ class TimeKeeper
   def set(key)
     new_params = {:key => key, :datetime => Time.new.to_s}
     self.write_json(new_params)
+  end
+
+  # workログをクリアし、時間の差分をhoursで戻す
+  def unset
+    params = load_json
+    time_from = Time.parse(params['datetime'])
+    time_to   = Time.new
+    diff_hours = self.getTimeDiff(time_from, time_to)
+    write_json({})
+    return diff_hours
+  end
+
+  # 2つのTimeオブジェクトの時刻差をhoursで戻す
+  def getTimeDiff(time_from, time_to)
+    diff_sec = (time_to - time_from).to_f
+    diff_sec / 60 / 60
+  end
+
+  # ログファイルをHashに読み込む
+  def load_json
+    File.open(@file_path) do |f|
+      JSON.load(f)
+    end
   end
 
   # ログファイルにJSONを書き出す

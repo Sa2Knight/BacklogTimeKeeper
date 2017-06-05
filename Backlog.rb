16c18fd5f63a6c215a254e79785eff3481c15ec9
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-05 21:28:02 +0900

    作業時間を取得するメソッドを追加

--- Backlog.rb
+++ Backlog.rb
@@ -1,11 +1,19 @@
 require 'backlog_kit'
+require 'pp'
 
 class Backlog
 
+  # スペースIDとAPIKeyを指定してオブジェクトを生成
   def initialize(space_id = nil, api_key = nil)
     space_id or space_id = ENV['BACKLOG_SPACE']
     api_key  or api_key  = ENV['BACKLOGAPI']
     @client = BacklogKit::Client.new(space_id: space_id, api_key: api_key)
+  end
+
+  # 課題キーを指定し、現在の作業時間を取得する
+  def getWorkingTime(key)
+    issue = @client.get_issue(key)
+    return issue.body.actualHours
   end
 
 end

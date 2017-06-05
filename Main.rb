7f94a272a98480f09eec418b975d1821411752d2
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-05 22:25:21 +0900

    メインスクリプトの雛形実装

--- Main.rb
+++ Main.rb
@@ -1,3 +1,18 @@
+require 'optparse'
 require_relative 'Backlog'
+require_relative 'TimeKeeper'
 
-backlog = Backlog.new
+# 課題をセットする
+def set(key)
+  tk = TimeKeeper.new
+  tk.set(key)
+end
+
+# 課題をアンセットする
+def unset
+
+end
+
+argv = ARGV.getopts('s:e')
+argv['s'] and set(argv['s'])
+argv['e'] and unset()

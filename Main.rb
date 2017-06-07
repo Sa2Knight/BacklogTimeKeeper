fd224852aab453155125b89946e34256c6d939b6
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-07 20:16:48 +0900

    子課題の作業時間の合計を親課題の作業時間に設定する機能を追加

--- Main.rb
+++ Main.rb
@@ -8,7 +8,7 @@ class Main
     @tk = TimeKeeper.new
   end
 
-  # 課題をセットする
+  # 機能1. 課題をセットする
   def set(key)
     backlog = Backlog.new(:issue_key => key)
     @tk.is_empty? or raise '課題が作業中です'
@@ -16,7 +16,7 @@ class Main
     puts "#{backlog.getTitle}(#{key})を開始しました"
   end
 
-  # 課題をアンセットする
+  # 機能2. 課題をアンセットする
   def unset
     @tk.is_empty? and raise '課題が設定されていません'
     result = @tk.unset
@@ -27,9 +27,16 @@ class Main
     puts "#{backlog.getTitle}(#{key})を終了し、作業時間を#{hours_to_add.round(2)}時間追加しました"
   end
 
+  # 機能3. 親課題の作業時間を記録する
+  def writeParentIssueWorkingTime(key)
+    backlog = Backlog.new(:issue_key => key)
+    backlog.setWorkingTime(backlog.getChildrenTotalWorkingTime)
+  end
+
 end
 
 main = Main.new
-argv = ARGV.getopts('s:e')
+argv = ARGV.getopts('s:ep:')
 argv['s'] and main.set(argv['s'])
 argv['e'] and main.unset()
+argv['p'] and main.writeParentIssueWorkingTime(argv['p'])

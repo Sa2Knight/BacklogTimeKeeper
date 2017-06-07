cde46d6e551f91751df7b943d3927f0aa0465365
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-08 00:23:08 +0900

    本日の課題毎の作業時間を集計する機能を実装

--- BacklogActivities.rb
+++ BacklogActivities.rb
@@ -6,24 +6,34 @@ class BacklogActivities < Backlog
   # Backlogクラスのイニシャライザを呼び、ユーザのアクティビティを取得する
   def initialize(params = {})
     super(params)
-    activities_params = {:activityTypeId => [2], :count => 10}
+    activities_params = {:activityTypeId => [2], :count => 100}
     @activities = @client.get_user_activities(@user.id, activities_params).body
   end
 
   # アクティビティから、作業時間の変更アクティビティを整形して抜き出す
-  def ActivitiesChangeWorkingTimes
+  def activitiesChangeWorkingTimes
     formatted_activities = []
     activities = @activities.each do |activity|
       changed_info = activity.content.changes.find {|c| c.field == 'actualHours'}
       changed_info or next
       formatted_activities.push({
-        :datetime  => Time.parse(activity.created),
+        :date      => Date.parse(activity.created),
         :issue_key => "#{activity.project.projectKey}-#{activity.content.key_id}",
         :old_value => changed_info.old_value.to_f,
         :new_value => changed_info.new_value.to_f,
       })
     end
     return formatted_activities
+  end
+
+  # 本日の課題毎の作業時間を集計する
+  def todaysTotalWorkingTimes
+    aggregate = Hash.new(0)
+    activities = self.activitiesChangeWorkingTimes
+    todays_activities = activities.select {|a| a[:date].to_s == Date.today.to_s}
+    todays_activities.each do |ac|
+      aggregate[ac[:issue_key]] += ac[:new_value] - ac[:old_value]
+    end
   end
 
 end

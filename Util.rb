82ec2a3f75d66284ae7d2f27b031a65e054eb4cb
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-06-19 00:37:12 +0900

    日付関係のロジックを集約したUtilクラスを実装

---
+++ Util.rb
@@ -0,0 +1,38 @@
+require 'date'
+class Util
+
+  # 日付文字列をDateオブジェクトに変換する
+  # 引数が既にDateの場合そのまま戻す
+  def self.strToDate(str)
+    str.class == Date   and return str
+    str.class == String and return Date.parse(str)
+    return nil
+  end
+
+  # Dateオブジェクトを文字列に変換する
+  # 引数が既に文字列の場合そのまま戻す
+  def self.dateToStr(date)
+    date.class == Date   and return date.to_s
+    date.class == String and return date
+  end
+
+  # 指定した日付が特定の期間に含まれているかを戻す
+  def self.periodIn?(date, date_from, date_to)
+    date      = Util.strToDate(date)
+    date_from = Util.strToDate(date_from)
+    date_to   = Util.strToDate(date_to)
+    return date_from <= date && date <= date_to
+  end
+
+  # 指定した日を含む週の月曜日と金曜日の日付を取得
+  # 週は月曜に始まり、土日で終了するものとする
+  def self.getWeeklyDate(date)
+    date  = Util.strToDate(date)
+    cwday = date.cwday
+    return {
+      date_from: date + (1 - cwday),
+      date_to:   date + (5 - cwday),
+    }
+  end
+
+end

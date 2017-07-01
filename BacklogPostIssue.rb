469c544e3dc6e6dc316e3134d557b1387ff65b0a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-07-01 19:01:56 +0900

    週間作業ログをBacklogに投稿するクラスを作成

---
+++ BacklogPostIssue.rb
@@ -0,0 +1,55 @@
+require 'date'
+require 'pp'
+require_relative 'Backlog'
+require_relative 'Util'
+
+class BacklogPostIssue < Backlog
+
+  def initialize(project_key, logs, params = {})
+    @project_key = project_key
+    @logs = logs
+    super(params)
+  end
+
+  private
+
+    # プロジェクト名からプロジェクトIDを取得する
+    def getProjectId(project_name)
+      project = @client.get_project(project_name)
+      project.body.id
+    end
+
+    # 課題タイトルを生成
+    def makeSummary
+      name = @user.name
+      date_from = @logs[:date_from].strftime('%m/%d')
+      date_to   = @logs[:date_to].strftime('%m/%d')
+      return "[#{name}] #{date_from}~#{date_to}"
+    end
+
+    # 課題本文を生成
+    def makeContent
+      contents = []
+      @logs[:projects].each do |key, val|
+        contents << makeProjectHeader(key)
+        contents.concat(makeProjectIssues(key))
+      end
+      contents.join("\n")
+    end
+
+    # プロジェクトごとの見出しを生成
+    def makeProjectHeader(project_key)
+      project = @logs[:projects][project_key]
+      return "** #{project_key} #{project[:total]}時間"
+    end
+
+    # 課題ごとの作業時間記録を生成
+    def makeProjectIssues(project_key)
+      issues = []
+      @logs[:projects][project_key][:issues].each do |i|
+        issues << "- #{i[:key]} #{i[:summary]} (#{i[:hours]}時間)"
+      end
+      issues
+    end
+
+end
